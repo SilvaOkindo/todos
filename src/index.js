bacac8fab170a8ba9a95d419fdbe2030e2d8c12e
silvaokindo <okindosilva@gmail.com>
2025-09-21 10:56:08 +0300

    new route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express()
 
+app.use("hello", (req, res) => {
+    res.json({message: "hello from render"})
+})
+
 app.listen(5000, () => {
     console.log("app running on port 5000")
 })
